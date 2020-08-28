4cf3401191079ba87ee126757f3e8d9ac187bbc4
Jack Whitesell <me@jackwhitesell.com>
2020-08-28 02:33:46 -0700

    completed switch to react

--- app/goals.js
+++ app/goals.js
@@ -1,4 +1,6 @@
-
+const react = require('react');
+const rd = require('react-dom');
+const $ = require('jquery');
 
 $(document).ready(function() {
   //connect to database (JSON file)
@@ -8,58 +10,50 @@ $(document).ready(function() {
   //creates the goals table if it doesn't already exist
   db.createTable(tableName, (succ, msg) => {});
 
-  //current goal being created
-  var obj;
-
   //creates a new goal object
   function newObject() {
-    obj = new Object();
-    obj.require = new Array();
-    obj.parents = new Array();
+    var o = new Object();
+    o.require = new Array();
+    o.parents = new Array();
+    return o;
   }
-
-  newObject();
 
   //display all current goals
   function displayAll() {
+    var current = [];
+    var future = [];
+    var reqList = [];
     db.getAll(tableName, (succ, data) => {
       data.forEach(function(element) {
-        appendToDatabase(element);
+        if (element.require.length > 0) {
+          future.push(e(Goal, {key: element.title, title: element.title, reqs: element.require}, null));
+        } else {
+          current.push(e(Goal, {key: element.title, title: element.title, reqs: element.require}, null));
+        }
+        reqList.push(e('option', {key: element.title, value: element.title}, element.title));
       });
     });
+
+    rd.render(
+      current,
+      document.getElementById("currentList")
+    );
+
+    // TODO: sort by number of requirements
+    rd.render(
+  		future,
+  		document.getElementById("futureList")
+  	);
+
+    rd.render(
+      reqList,
+      document.getElementById("requireList")
+    );
   }
 
+  //current goal being created
+  var obj = newObject();
   displayAll();
-
-  //adds a goal to the page
-  function appendToDatabase(element) {
-    var reqs = "<ul class='reqList subtitle is-size-6 is-uppercase'>";
-    element.require.forEach((item, i) => {
-      reqs = reqs + "<li>" + item + "</li>";
-    });
-    addRequirements(element, ".requireList");
-
-    var databaseName;
-    if (reqs.includes("<li>")) {
-      databaseName = "#futureList"
-    } else {
-      databaseName = "#currentList"
-    }
-    reqs = reqs + "</ul>";
-    var title = "<p>" + element.title + "</p>";
-    var edit = "<input type='button' class='editButton button is-danger' value='Edit'>";
-    var select = "<div style='display:none;' class='editSelect'><input type='button' value='Confirm Edit' class='addReqEdit button is-danger'></div>";
-    var deleteButton = "<button class='delete deleteButton'></button>";
-    $("<div class='notification is-link is-light goal'>" +
-        "<h1 class='title is-uppercase is-size-5'>" + title + "</h1>"
-        + reqs + edit + select + deleteButton + "</div>").appendTo(databaseName);
-
-  }
-
-  //add new goal to requirements form field
-  function addRequirements(element, where) {
-    $("<option value='"+element.title+"'>"+element.title+"</option>").appendTo(where + ":first");
-  }
 
   //updates the parent/require array of an element
   //true for parent, false for requirement
@@ -76,7 +70,6 @@ $(document).ready(function() {
           setArray.push(newTitle);
           set = {"parents": setArray};
         } else {
-          console.log(result);
           setArray = result[0].require;
           setArray.push(newTitle);
           set = {"require": setArray};
@@ -91,9 +84,6 @@ $(document).ready(function() {
 
   //refreshes all elements on the page with updated information after an add/edit call
   function refresh() {
-    $("#currentList").html("");
-    $("#futureList").html("");
-    $(".requireList:first").html("");
     displayAll();
   }
 
@@ -115,7 +105,7 @@ $(document).ready(function() {
     db.getField(tableName, type, (succ, data) => {
       if (succ) {
         data.forEach(function(element, i) {
-          element = jQuery.grep(element, function(value) {
+          element = $.grep(element, function(value) {
             return value != selection;
           });
           data[i] = element;
@@ -145,10 +135,7 @@ $(document).ready(function() {
   //edit goal
   $(document).on("click", ".addReqEdit", function() {
     var selection = $(this).siblings(".requireDiv").find("option:selected").val();
-    console.log(selection);
     var current = $(this).parents(".goal").find("h1").text();
-    console.log(current);
-    $("<li>" + selection + "</li>").appendTo($(this).parent(".goal").find(".reqList"));
     updateArray(current, selection, false);
     updateArray(selection, current, true);
     refresh();
@@ -156,7 +143,7 @@ $(document).ready(function() {
 
   //add currently selected requirement to the list and display on page
   $("#reqButton").click(function() {
-    var selection = $(".requireList:first").val();
+    var selection = $("#requireList:first").val();
     $("<p>" + selection + "</p>").appendTo("#currentReqs");
     obj.require.push(selection);
   });
@@ -167,50 +154,37 @@ $(document).ready(function() {
     db.insertTableContent(tableName, obj, (succ, msg) => {
       // succ - boolean, tells if the call is successful
       if (succ) {
-        appendToDatabase(obj);
         obj.require.forEach(function(element) {
           if (element != "none") {
             updateArray(element, obj.title, true);
           }
         });
+        displayAll();
         $("#currentReqs").html("");
+        $(".newType").hide();
+        $(".addNew").show();
       }
     });
     newObject();
   });
-
-  function displayAll2() {
-    var list = new Array();
-    db.getAll(tableName, (succ, data) => {
-      data.forEach(function(element) {
-        list.push(e(Goal, {title: element.title, reqs: element.require}, null));
-      });
-    });
-    return list;
-  }
-
-  ReactDOM.render(
-  		displayAll2(),
-  		document.getElementById("reactList")
-  	);
 });
 
-const e = React.createElement;
+const e = react.createElement;
 
-class Goal extends React.Component {
+class Goal extends react.Component {
   render() {
+    var list = [];
+    react.Children.map(this.props.reqs, item => {
+      list.push(e("li", {key: item}, item));
+    });
 
     return e('div', {className: 'notification is-link is-light goal'},
               e('h1', {className: 'title is-uppercase is-size-5'}, `${this.props.title}`),
-              e('ul', {className: 'reqList subtitle is-size-6 is-uppercase'}, null)
+              e('ul', {className: 'reqList subtitle is-size-6 is-uppercase'}, list),
+              e('input', {className: 'editButton button is-danger', type: 'button', value: 'Edit'}, null),
+              e('div', {className: "hidden editSelect"},
+                  e('input', {className: "addReqEdit button is-danger", type: "button", value: "Confirm Edit"}, null)),
+              e('button', {className: "delete deleteButton"}, null)
             );
   }
 }
-
-// function Goal(props) {
-//   return (
-//     <div className='notification is-link is-light goal'>
-//       <h1 className='title is-uppercase is-size-5'>{props.title}</h1>
-//     </div>
-//   );
-// }
