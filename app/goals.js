14516fc812c4e7ccc7b01f578014b8c87d62c033
Jack Whitesell <me@jackwhitesell.com>
2020-08-29 21:22:29 -0700

    form UI and functionality edits

--- app/goals.js
+++ app/goals.js
@@ -24,45 +24,47 @@ $(document).ready(function() {
   function displayAll() {
     var current = [];
     var future = [];
+    var reqList = [];
     var map = new Map();
-    db.all("SELECT title, requires FROM goals ORDER BY title ASC", function(err, rows) {
-      rows.forEach(function(row) {
-        if (map.has(row.title)) {
-          var temp = map.get(row.title);
-          temp.push(row.requires);
-          map.set(row.title, temp);
-        } else {
-          map.set(row.title, [row.requires]);
-        }
+    db.serialize(function() {
+      db.all("SELECT name FROM names ORDER BY name ASC", function(err, rows) {
+        rows.forEach(function(row) {
+          reqList.push(e('option', {key: row.name, value: row.name}, row.name));
+        });
+        rd.render(
+          reqList,
+          document.getElementById("requireList")
+        );
       });
-      map.forEach(function(value, key) {
-        if (value.length == 1) {
-          current.push(e(Goal, {key: key, title: key, reqs: []}, null));
-        } else {
-          future.push(e(Goal, {key: key, title: key, reqs: value}, null));
-        }
-      });
-      console.log(map);
 
-      rd.render(
-        current,
-        document.getElementById("currentList")
-      );
+      db.all("SELECT title, requires FROM goals ORDER BY title ASC", function(err, rows) {
+        rows.forEach(function(row) {
+          if (map.has(row.title)) {
+            var temp = map.get(row.title);
+            temp.push(row.requires);
+            map.set(row.title, temp);
+          } else {
+            map.set(row.title, [row.requires]);
+          }
+        });
+        map.forEach(function(value, key) {
+          if (value.length == 1) {
+            current.push(e(Goal, {key: key, title: key, reqs: [], select: reqList}, null));
+          } else {
+            future.push(e(Goal, {key: key, title: key, reqs: value.slice(1), select: reqList}, null));
+          }
+        });
 
-      rd.render(
-    		future,
-    		document.getElementById("futureList")
-    	);
-    });
-    db.all("SELECT name FROM names ORDER BY name ASC", function(err, rows) {
-      var reqList = [];
-      rows.forEach(function(row) {
-        reqList.push(e('option', {key: row.name, value: row.name}, row.name));
+        rd.render(
+          current,
+          document.getElementById("currentList")
+        );
+
+        rd.render(
+      		future,
+      		document.getElementById("futureList")
+      	);
       });
-      rd.render(
-        reqList,
-        document.getElementById("requireList")
-      );
     });
   }
 
@@ -72,14 +74,21 @@ $(document).ready(function() {
 
   //show the goal form after clicking the add button
   $("#add").click(function() {
-    $(".addNew").hide();
+    $("#add").hide();
     $(".newType").show();
+  });
+
+  //cancels adding a new goal to the datbase
+  $("#cancel").click(function() {
+    $("#add").show();
+    $(".newType").hide();
+    $("#currentReqs").html("");
   });
 
   //show edit form
   $(document).on("click", ".editButton", function() {
     $(this).hide();
-    $(".requireDiv:first").clone().prependTo($(this).siblings(".editSelect"));
+    $(this).siblings(".reqList").find("input").show();
     $(this).siblings(".editSelect").show();
   });
 
@@ -91,7 +100,7 @@ $(document).ready(function() {
     displayAll();
   });
 
-  //edit goal
+  //add requirement to goal
   $(document).on("click", ".addReqEdit", function() {
     var stmt = db.prepare("INSERT INTO goals VALUES (?, ?)");
     stmt.run($(this).parents(".goal").find("h1").text(), $(this).siblings(".requireDiv").find("option:selected").val());
@@ -102,8 +111,23 @@ $(document).ready(function() {
   //add currently selected requirement to the list and display on page
   $("#reqButton").click(function() {
     var selection = $("#requireList:first").val();
-    $("<p>" + selection + "</p>").appendTo("#currentReqs");
+    $("<li>" + selection + "</li>").appendTo("#currentReqs");
     obj.require.push(selection);
+  });
+
+  //removes a requirement from a goal
+  $(document).on("click", ".removeButton", function() {
+    var stmt = db.prepare("DELETE FROM goals WHERE requires == (?)");
+    stmt.run($(this).parents("li").text());
+    stmt.finalize();
+    displayAll();
+  });
+
+  //cancels editing the goal
+  $(document).on("click", ".cancelButton", function() {
+    $(this).parents(".editSelect").hide();
+    $(this).parents(".notification").find(".removeButton").hide();
+    $(this).parents(".notification").find(".editButton").show();
   });
 
   //add the goal to the database
@@ -130,24 +154,27 @@ $(document).ready(function() {
   });
 });
 
-window.onbeforeunload = function(){
-  db.close();
-};
-
 class Goal extends react.Component {
   render() {
     var list = [];
     react.Children.map(this.props.reqs, item => {
-      list.push(e("li", {key: item}, item));
+      list.push(e("li", {key: item},
+        item, e('input', {className: "removeButton hidden", type: "button", value: "Remove requirement"}, null)
+      ));
     });
 
     return e('div', {className: 'notification is-link is-light goal'},
-              e('h1', {className: 'title is-uppercase is-size-5'}, `${this.props.title}`),
-              e('ul', {className: 'reqList subtitle is-size-6 is-uppercase'}, list),
-              e('input', {className: 'editButton button is-danger', type: 'button', value: 'Edit'}, null),
-              e('div', {className: "hidden editSelect"},
-                  e('input', {className: "addReqEdit button is-danger", type: "button", value: "Confirm Edit"}, null)),
-              e('button', {className: "delete deleteButton"}, null)
+             e('h1', {className: 'title is-uppercase is-size-5'}, `${this.props.title}`),
+             e('ul', {className: 'reqList subtitle is-size-6 is-uppercase'}, list),
+               e('input', {className: 'editButton button is-danger', type: 'button', value: 'Edit'}, null),
+               e('div', {className: "hidden editSelect"},
+                 e('div', {className: "requireDiv select is-primary"},
+                   e('select', {name: "require"}, this.props.select)
+                  ),
+                 e('input', {className: "addReqEdit button is-danger", type: "button", value: "Confirm Edit"}, null),
+                 e('input', {className: "cancelButton button is-danger", type: "button", value: "Cancel"}, null)
+                ),
+               e('button', {className: "delete deleteButton hidden"}, null)
             );
   }
 }
