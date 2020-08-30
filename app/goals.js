f41e3ab3645698549a65499fba7bff8c1e272352
Jack Whitesell <me@jackwhitesell.com>
2020-08-29 17:47:12 -0700

    completed switch to sqlite

--- app/goals.js
+++ app/goals.js
@@ -1,31 +1,17 @@
 const react = require('react');
 const rd = require('react-dom');
 const $ = require('jquery');
-const db = require('electron-db');
 var sqlite3 = require('sqlite3').verbose();
-var db2 = new sqlite3.Database('goals.db');
+var db = new sqlite3.Database('goals.db');
 
 const e = react.createElement;
 
 $(document).ready(function() {
-  //connect to database (JSON file)
-  const tableName = "goals";
-
-  db2.serialize(function() {
-    db2.run("CREATE TABLE IF NOT EXISTS goals (title TEXT, requires TEXT)");
-
-    // db2.each("SELECT nameid AS id, name FROM names", function(err, row) {
-    //     console.log(row.id + ": " + row.name);
-    // });
-    //
-    // db2.each("SELECT goalid AS id, title FROM goals", function(err, row) {
-    //     console.log(row.id + ": " + row.title);
-    // });
+  db.serialize(function() {
+    db.run("CREATE TABLE IF NOT EXISTS goals (title TEXT, requires TEXT, CONSTRAINT fk_title FOREIGN KEY (title) REFERENCES names(name) ON DELETE CASCADE, CONSTRAINT fk_req FOREIGN KEY (requires) REFERENCES names(name) ON DELETE CASCADE)");
+    db.run("CREATE TABLE IF NOT EXISTS names (name TEXT PRIMARY KEY)");
+    db.run("PRAGMA foreign_keys = ON");
   });
-
-  //creates the goals table if it doesn't already exist
-  db.createTable(tableName, (succ, msg) => {});
-
   //creates a new goal object
   function newObject() {
     var o = new Object();
@@ -38,37 +24,46 @@ $(document).ready(function() {
   function displayAll() {
     var current = [];
     var future = [];
-    var reqList = [];
-    db.getAll(tableName, (succ, data) => {
-      data.forEach(function(element) {
-        if (element.require.length > 0) {
-          future.push(e(Goal, {key: element.title, title: element.title, reqs: element.require}, null));
+    var map = new Map();
+    db.all("SELECT title, requires FROM goals ORDER BY title ASC", function(err, rows) {
+      rows.forEach(function(row) {
+        if (map.has(row.title)) {
+          var temp = map.get(row.title);
+          temp.push(row.requires);
+          map.set(row.title, temp);
         } else {
-          current.push(e(Goal, {key: element.title, title: element.title, reqs: element.require}, null));
+          map.set(row.title, [row.requires]);
         }
-        reqList.push(e('option', {key: element.title, value: element.title}, element.title));
       });
+      map.forEach(function(value, key) {
+        if (value.length == 1) {
+          current.push(e(Goal, {key: key, title: key, reqs: []}, null));
+        } else {
+          future.push(e(Goal, {key: key, title: key, reqs: value}, null));
+        }
+      });
+      console.log(map);
+
+      rd.render(
+        current,
+        document.getElementById("currentList")
+      );
+
+      rd.render(
+    		future,
+    		document.getElementById("futureList")
+    	);
     });
-
-    db2.each("SELECT title, requires FROM goals", function(err, row) {
-        console.log(row.title + " : " + row.requires);
+    db.all("SELECT name FROM names ORDER BY name ASC", function(err, rows) {
+      var reqList = [];
+      rows.forEach(function(row) {
+        reqList.push(e('option', {key: row.name, value: row.name}, row.name));
+      });
+      rd.render(
+        reqList,
+        document.getElementById("requireList")
+      );
     });
-
-    rd.render(
-      current,
-      document.getElementById("currentList")
-    );
-
-    // TODO: sort by number of requirements
-    rd.render(
-  		future,
-  		document.getElementById("futureList")
-  	);
-
-    rd.render(
-      reqList,
-      document.getElementById("requireList")
-    );
   }
 
   //current goal being created
@@ -88,42 +83,19 @@ $(document).ready(function() {
     $(this).siblings(".editSelect").show();
   });
 
-  //remove item from every require/parent array
-  function removeField(selection, type) {
-    db.getField(tableName, type, (succ, data) => {
-      if (succ) {
-        data.forEach(function(element, i) {
-          element = $.grep(element, function(value) {
-            return value != selection;
-          });
-          data[i] = element;
-        });
-        db.getAll(tableName, (succ2, data2) => {
-          data2.forEach(function(element, i) {
-            if (element[type] != data[i]) {
-              var where = {"title": element.title};
-              var set = {[type]: data[i]};
-              db.updateRow(tableName, where, set, (succ, msg) => {});
-            }
-          });
-        });
-      }
-    });
-  }
-
   //delete goal
   $(document).on("click", ".deleteButton", function() {
-    var selection = $(this).siblings("h1").text();
-    removeField(selection, "require");
-    removeField(selection, "parents");
-    db.deleteRow(tableName, {"title": selection}, (succ, msg) => {});
+    var stmt = db.prepare("DELETE FROM names WHERE name == (?)");
+    stmt.run($(this).siblings("h1").text());
+    stmt.finalize();
     displayAll();
   });
 
   //edit goal
   $(document).on("click", ".addReqEdit", function() {
-    var stmt = db2.prepare("INSERT INTO goals VALUES (?, ?)");
+    var stmt = db.prepare("INSERT INTO goals VALUES (?, ?)");
     stmt.run($(this).parents(".goal").find("h1").text(), $(this).siblings(".requireDiv").find("option:selected").val());
+    stmt.finalize();
     displayAll();
   });
 
@@ -139,14 +111,28 @@ $(document).ready(function() {
     $("#currentReqs").html("");
     $(".newType").hide();
     $(".addNew").show();
-    var stmt = db2.prepare("INSERT INTO goals VALUES (?, ?)");
-    obj.require.forEach(function(element) {
-      stmt.run($("#skillText").val(), element);
+    db.serialize(function() {
+      var value = $("#skillText").val();
+      var stmt = db.prepare("INSERT INTO names VALUES (?)");
+      stmt.run(value)
+      stmt = db.prepare("INSERT INTO goals VALUES (?, ?)");
+      stmt.run(value, null);
+      if (obj.require.length != 0) {
+        stmt = db.prepare("INSERT INTO goals VALUES (?, ?)");
+        obj.require.forEach(function(element) {
+          stmt.run(value, element);
+        });
+      }
+      stmt.finalize();
+      displayAll();
+      obj = newObject();
     });
-    displayAll();
-    newObject();
   });
 });
+
+window.onbeforeunload = function(){
+  db.close();
+};
 
 class Goal extends react.Component {
   render() {
