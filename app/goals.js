bfbe263b0653cec91bf781bcb2440fb0fb0752e0
Jack Whitesell <me@jackwhitesell.com>
2020-08-30 18:58:14 -0700

    started on timers

--- app/goals.js
+++ app/goals.js
@@ -3,6 +3,7 @@ const rd = require('react-dom');
 const $ = require('jquery');
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('goals.db');
+const moment = require('moment')
 
 const e = react.createElement;
 
@@ -11,6 +12,7 @@ $(document).ready(function() {
   db.serialize(function() {
     db.run("CREATE TABLE IF NOT EXISTS goals (title TEXT, requires TEXT, CONSTRAINT fk_title FOREIGN KEY (title) REFERENCES names(name) ON DELETE CASCADE, CONSTRAINT fk_req FOREIGN KEY (requires) REFERENCES names(name) ON DELETE CASCADE)");
     db.run("CREATE TABLE IF NOT EXISTS names (name TEXT PRIMARY KEY)");
+    db.run("CREATE TABLE IF NOT EXISTS timers (name TEXT PRIMARY KEY, time TEXT)");
     db.run("PRAGMA foreign_keys = ON");
   });
 
@@ -29,6 +31,7 @@ $(document).ready(function() {
     var future = [];
     var reqList = [];
     var map = new Map();
+
     db.serialize(function() {
       db.all("SELECT name FROM names ORDER BY name ASC", function(err, rows) {
         rows.forEach(function(row) {
@@ -64,19 +67,42 @@ $(document).ready(function() {
           document.getElementById("currentList")
         );
 
+        // sort future goals list by number of requirements
+        future.sort(function(a, b){
+          return a.props.reqs.length - b.props.reqs.length;
+        });
         rd.render(
       		future,
       		document.getElementById("futureList")
       	);
       });
+
+
     });
     obj = newObject();
     obj.reqList = reqList;
   }
 
+  function displayTimers() {
+    var timers = [];
+    db.all("SELECT name, time FROM timers", function(err, rows) {
+      rows.forEach(function(row) {
+        timers.push(e(Timer, {key: row.name, title: row.name, time: row.time}, null));
+      });
+
+      console.log(timers);
+      timers = timers.filter(i => i != null);
+      rd.render(
+        timers,
+        document.getElementById("timerList")
+      );
+    });
+  }
+
   //current goal being created
   var obj;
   displayAll();
+  displayTimers();
 
   //show the goal form after clicking the add button
   $("#add").click(function() {
@@ -108,7 +134,7 @@ $(document).ready(function() {
   //delete goal
   $(document).on("click", ".deleteButton", function() {
     var stmt = db.prepare("DELETE FROM names WHERE name == (?)");
-    stmt.run($(this).siblings("h1").text());
+    stmt.run($(this).siblings("h2").text());
     stmt.finalize();
     displayAll();
   });
@@ -116,7 +142,7 @@ $(document).ready(function() {
   //add requirement to goal
   $(document).on("click", ".addReqEdit", function() {
     var stmt = db.prepare("INSERT INTO goals VALUES (?, ?)");
-    stmt.run($(this).parents(".goal").find("h1").text(), $(this).siblings(".requireDiv").find("option:selected").val());
+    stmt.run($(this).parents(".goal").find("h2").text(), $(this).siblings(".requireDiv").find("option:selected").val());
     stmt.finalize();
     displayAll();
   });
@@ -156,7 +182,6 @@ $(document).ready(function() {
     $("#currentReqs").html("");
     $(".newType").hide();
     $("#add").show();
-    console.log(obj.require);
     var stmt;
     db.serialize(function() {
       var value = $("#skillText").val();
@@ -180,6 +205,27 @@ $(document).ready(function() {
       stmt.finalize();
     });
   });
+
+  $("#timerButton").click(function() {
+    var stmt = db.prepare("INSERT INTO timers VALUES (?, ?)");
+    stmt.run($("#timerTitle").val(), $("#timerDate").val() + " " + $("#timerTime").val());
+    stmt.finalize();
+    $("#timerTitle").val("");
+    $("#timerDate").val("");
+    $("#timerTime").val("");
+  });
+
+  setInterval(function() {
+    var d1 = new Date();
+    $(".time").each(function() {
+      var d2 = new Date($(this).text());
+      var n = d2.getTime() - d1.getTime();
+      if (n < 0) {
+        alert("Timer for " + $(this).siblings("h2").text() + " is done");
+      }
+    });
+    displayTimers();
+  }, 900000);
 });
 
 class Goal extends react.Component {
@@ -196,7 +242,7 @@ class Goal extends react.Component {
       : null;
 
     return e('div', {className: 'notification is-link is-light goal'},
-             e('h1', {className: 'title is-uppercase is-size-5'}, `${this.props.title}`),
+             e('h2', {className: 'title is-uppercase is-size-5'}, `${this.props.title}`),
              e('ul', {className: 'reqList subtitle is-size-6 is-uppercase'}, list),
                e('input', {className: 'editButton button is-danger', type: 'button', value: 'Edit'}, null),
                e('div', {className: "hidden editSelect"},
@@ -208,6 +254,19 @@ class Goal extends react.Component {
   }
 }
 
+class Timer extends react.Component {
+  render() {
+    var d = moment(new Date(this.props.time)).format("ddd MMM Do [at] HH:mm");
+    var u = moment(d, "ddd MMM Do [at] HH:mm").fromNow();
+
+    return e('div', {className: 'notification is-link is-light timer'},
+              e('h2', {className: 'title is-uppercase is-size-5'}, `${this.props.title}`),
+              e('p', {className: 'subtitle is-size-6 time'}, d.toString()),
+              e('p', {className: 'subtitle is-size-6 until'}, u.toString())
+            );
+  }
+}
+
 // work on timers
-// data checks
-//    no loop dependencies
+// data checks for timers
+// display goals with more reqs lower on list (done)
